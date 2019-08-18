8023349b4e25c9ae0cb911f641c37f1dd064e0a2
xuqi <xuqi@changba.com>
2019-08-18 17:18:58 +0800

    引用发布后的本地仓库

--- app/src/main/java/com/example/androidlibdemo/MainActivity.java
+++ app/src/main/java/com/example/androidlibdemo/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.androidlibdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.androidxuqiutillib.AndroidLibUtil;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "lib return = " + AndroidLibUtil.getUserName());
     }
 }
